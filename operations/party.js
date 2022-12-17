5ad9aed13bad119aa4ed7f823492807eae083216
BuildTools <unconfigured@null.spigotmc.org>
2022-12-17 11:56:39 -0500

    st  tropez party girls

---
+++ operations/party.js
@@ -0,0 +1,11 @@
+const { register_operation, broadcast, construct_packet } = require("../packet_parser")
+
+// SHARED [0x20 -> 0x30] Party Packets
+function slay(packet, ws, clients) {
+
+
+}
+
+exports.bind = () => {
+    
+}
